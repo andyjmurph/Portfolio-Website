547b07ba3958435cf75341834a0fc4890ca9361b
andyjmurph <andyjmurph@icloud.com>
2017-08-12 22:29:13 +0100

    Drop down navigation in mobile view

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 "use strict";
+
+$(document).ready(function () {
+
+  // Global variables
+  var $navBtn = $('.nav-btn');
+  var $mainNav = $('.main-nav');
+
+  // .nav-btn button click event
+  $navBtn.click(function () {
+    $(this).toggleClass('open');
+    $mainNav.toggleClass('open');
+    if ($mainNav.hasClass('open')) {
+      $mainNav.slideDown('fast');
+    } else {
+      $mainNav.slideUp('fast');
+    }
+  });
+
+  // End of 'document.ready' statement.
+});
