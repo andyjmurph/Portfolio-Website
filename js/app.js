83cbde81a5e50e98e2e39f91681f2ed48de9f5bf
andyjmurph <andyjmurph@icloud.com>
2017-08-13 20:55:07 +0100

    Added smooth scrolling

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
   // Global variables
   var $navBtn = $('.nav-btn');
   var $mainNav = $('.main-nav');
+  var $body = $('body');
+  var $html = $('html');
 
   // .nav-btn button click event
   $navBtn.click(function () {
@@ -16,6 +18,13 @@ $(document).ready(function () {
       $mainNav.slideUp('fast');
     }
   });
+
+  $(".main-nav, .footer-nav").on('click', 'a', function(event){
+    event.preventDefault();
+    $html.add($body).animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
 
   // End of 'document.ready' statement.
 });
